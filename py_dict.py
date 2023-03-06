3b1867ff7c372c1ffd3a6a0ebe75c8597709dd08
Gabrijel <g.gordic@outlook.com>
2023-03-06 20:37:54 +0100

    svasta

---
+++ py_dict.py
@@ -0,0 +1,43 @@
+employees = {
+# kljuc ili key    vrijednost ili value
+    '11111111' : 'Pero Peric',
+    '22222222' : 'Ana Anic',
+    '33333333' : 'Marko Maric' 
+}
+
+dict_element = employees['22222222']
+print(dict_element)
+
+print()
+print('dict keys')
+py_dict_keys = employees.keys()
+print(py_dict_keys)
+print()
+
+for key in employees.keys():
+    print(key)
+    print(employees[key])
+    
+
+print()
+print('dict values')
+py_dict_values = employees.values()
+print(py_dict_values)
+print()
+
+for value in employees.values():
+    print(value)
+print()
+
+print()
+print('dict items')
+py_dict_items = employees.items()
+print(py_dict_items)
+
+print()
+#for item in employees.items():
+    #print(item)
+    
+for key, value in employees.items():
+    print(key, value)
+print()
