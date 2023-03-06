0ca9d41d11d5c748b1d297043785990111de9f08
Gabrijel <g.gordic@outlook.com>
2023-03-06 20:54:51 +0100

    Update py_demo.py

--- py_demo.py
+++ py_demo.py
@@ -1,15 +1,20 @@
 
+# names = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
+# 
+# pero,ana,marko,mirko,slavko = names 
+# 
+# print(pero)
+# print(ana)
+# print(marko)
+# print(mirko)
+# print(slavko)
+# 
+# names_duplicate = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
+# 
+# names_duplicate = pero,ana,marko,mirko,slavko
+# print(names_duplicate)
+
 names = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
 
-pero,ana,marko,mirko,slavko = names 
-
-print(pero)
-print(ana)
-print(marko)
-print(mirko)
-print(slavko)
-
-names_duplicate = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
-
-names_duplicate = pero,ana,marko,mirko,slavko
-print(names_duplicate)
+pero, ana, *ostatak = names
+print(ostatak)
