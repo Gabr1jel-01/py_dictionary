bc6b0d3f07b45ae4a1c5bd7d2c345793517d44dc
Gabrijel <g.gordic@outlook.com>
2023-03-06 20:51:21 +0100

    Create py_demo.py

---
+++ py_demo.py
@@ -0,0 +1,15 @@
+
+names = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
+
+pero,ana,marko,mirko,slavko = names 
+
+print(pero)
+print(ana)
+print(marko)
+print(mirko)
+print(slavko)
+
+names_duplicate = ['Pero', 'Ana', 'Marko', 'Mirko', 'Slavko']
+
+names_duplicate = pero,ana,marko,mirko,slavko
+print(names_duplicate)
